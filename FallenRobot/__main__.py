50948ca9ead3f372f5a5aa7b57caff88c7906f33
adarshUC <121399431+adarshUC@users.noreply.github.com>
2023-05-03 17:03:40 +0530

    Update __main__.py
    Signed-off-by: adarshUC <121399431+adarshUC@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -92,7 +92,7 @@ buttons = [
         InlineKeyboardButton(text="ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help_back"),
     ],
     [
-        InlineKeyboardButton(text="❄ ᴏᴡɴᴇʀ ❄", url=f"https://t.me/pyaein"),
+        InlineKeyboardButton(text="❄ ᴏᴡɴᴇʀ ❄", url=f"https://t.me/{OWNER_ID}"),
         InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
 ]
