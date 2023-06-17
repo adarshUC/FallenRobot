eefa937e1fc4838cb5441cfbc9b1982b3818e523
adarshUC <adarshuc595@gmail.com>
2023-06-17 12:27:08 +0530

    Update alive.py
    Signed-off-by: adarshUC <adarshuc595@gmail.com>

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -17,7 +17,7 @@ async def awake(_, message: Message):
     BUTTON = [
         [
             InlineKeyboardButton("ʜᴇʟᴘ", url=f"https://t.me/{BOT_USERNAME}?start=help"),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/scenario_support"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/XtromSupport"),
         ]
     ]
     await message.reply_photo(
