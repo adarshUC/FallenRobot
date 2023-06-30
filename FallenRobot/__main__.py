ec508b83cd2e120e2272381f3bece90af1841cd2
adarshUC <adarshuc595@gmail.com>
2023-06-30 16:13:23 +0530

    Update __main__.py
    Signed-off-by: adarshUC <adarshuc595@gmail.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -206,7 +206,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAIGlGSNP59KDzMYR0C5wq9prReH8FMWAALNCAACHPpwVFlbpRFQv2uhLwQ"
+                "CAACAgUAAx0CcssfsQAC8JJknrGoX8jD1UdJFfL0hr4614CtcwACHAgAAmzCuFbi-BJmRJKvFi8E"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
